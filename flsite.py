b2f494497fb9d56515f49a2ffdb656f73a467d45
mac <atk5549@yandex.ru>
2023-10-17 10:52:33 +1000

    [EDIT] Removed extra code

--- flsite.py
+++ flsite.py
@@ -1,104 +1,62 @@
 import os
+import psycopg2
+import requests
 from datetime import datetime
-from flask import Flask, jsonify, render_template
-
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import psycopg2
-
 from dotenv import load_dotenv
 load_dotenv()
 
 
-# DESERIALAZED DATA
-from apispec.ext.marshmallow import MarshmallowPlugin
-from apispec import APISpec
-from flask_apispec.extension import FlaskApiSpec
-
-import requests
-
-
 app = Flask(__name__)
 
+
 URLDB = os.getenv("DATABASE_URL")
-connection = psycopg2.connect(URLDB)
-if connection:
-    print("connection suessfully!!!")
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = URLDB
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
+connection = psycopg2.connect(URLDB)
+if connection:
+    print("========================================================================")
+    print("connection suessfully!!!")
+    print("========================================================================")
+
+
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-### команда для миграции субд
-### dependency:
-### create environment var:
-### 
-### Flask-Migrate==3.1.0
-### flask db init 
-### flask db migrate
-### flask db upgrade
 
 class QuestionForDB(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    id_question = db.Column(db.Integer)
     text_question = db.Column(db.String(1024), unique=False, nullable=False)
     text_answer = db.Column(db.String(1024), nullable=False)
-    pub_question = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    created_at_question = db.Column(db.DateTime, nullable=False)
+    updated_at_question = db.Column(db.DateTime, nullable=False)
+    saved_question = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     
     def __repr__(self):
         return '<QuestionForDB %r>' % self.text_question
-
-
-docs = FlaskApiSpec()
-docs.init_app(app)
-app.config.update({
-    'APISPEC_SPEC': APISpec(
-        title='videoblog',
-        version='v1',
-        openapi_version='2.0',
-        plugins=[MarshmallowPlugin()],
-    ),
-    'APISPEC_SWAGGER_URL': '/swagger/'
-})
-
-headers = {
-    "Content-Type": "application/json",
-}
-
-
-
-postinfo = [
     
-    {
-    "id": 0,
-    "post": "postinfo1"
-    },
-    {
-    "id": 1,
-    "post": "postinfo2"
-    }
-]
-
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="Главная", data=QuestionForDB.query.all())
-
-@app.route("/api/posts/")
-def posts():
-    return jsonify(postinfo)
-
+    return render_template("index.html",
+                           title="Главная",
+                           data=QuestionForDB.query.all())
 
 
 @app.route("/api/posts/<int:post_number>")
 def postsItem(post_number):
-    response = requests.get(f"https://jservice.io/api/random?count={post_number}", headers=headers)
+    response = requests.get(f"https://jservice.io/api/random?count={post_number}",
+                            headers={"Content-Type": "application/json"})
     res = response.json()
+    print("========================================================================")
     print(res[0]["id"])
     print(res[0]["question"])
     print(res[0]["answer"])
+    print("========================================================================")
     
     # ins = QuestionForDB(
     #     id=post_number,
@@ -126,3 +84,12 @@ if __name__ == "__main__":
 # pip install --only-binary :all: Flask-SQLAlchemy
 
 # Python packages are often distributed as "wheels" (.whl files), which are pre-compiled binary packages. Try installing "greenlet" and "Flask-SQLAlchemy" using wheels instead of source distribution
+
+### команда для миграции субд
+### dependency:
+### create environment var:
+### 
+### Flask-Migrate==3.1.0
+### flask db init 
+### flask db migrate
+### flask db upgrade
